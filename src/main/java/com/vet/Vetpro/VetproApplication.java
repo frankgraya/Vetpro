34d89cdf02139cbb75f24d28e4bd8974feaeab62
frank <francico.graya26@gmail.com>
2022-11-27 17:23:51 -0600

    first commit

---
+++ src/main/java/com/vet/Vetpro/VetproApplication.java
@@ -0,0 +1,13 @@
+package com.vet.Vetpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vet/Vetpro/VetproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vet.Vetpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
