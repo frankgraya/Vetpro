fa4924d761f8018f1aa4f0a625ccdf129f4ae9a1
frank <francico.graya26@gmail.com>
2022-12-06 22:43:30 -0600

    se agrego comando

--- src/main/java/com/vet/Vetpro/VetproApplication.java
+++ src/main/java/com/vet/Vetpro/VetproApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VetproApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(VetproApplication.class, args);
 	}
